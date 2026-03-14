b642693204ab2bca26cbee61ff38ac0deb938df9
Mirjalol <nosirovmirjalol869@gmail.com>
2026-03-14 19:46:10 +0500

    add

--- src/features/collage/collage.service.ts
+++ src/features/collage/collage.service.ts
@@ -10,7 +10,7 @@ export const collageService = {
 	create(data: CreateCollageDTO) {
 		return apiClient.post<CollageCreateResponse>(COLLAGE.CREATE, data);
 	},
-	delete(id:number,endPoint:string){
-		return apiClient.delete(`/${endPoint}/${id}`)
+	delete(id:number){
+		return apiClient.delete(`/college/${id}`)
 	}
 };
