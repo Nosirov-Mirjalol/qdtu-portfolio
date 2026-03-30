dac109e87f319a5444febad746932dd42bd5af34
Mirjalol <nosirovmirjalol869@gmail.com>
2026-03-30 21:08:54 +0500

    writes edit funtion in service

--- src/features/research/research.service.ts
+++ src/features/research/research.service.ts
@@ -1,9 +1,12 @@
 import { apiClient } from "@/api/client"
 import { RESEARCH } from "@/constants/apiEndpoint"
-import { GetByIdResponse } from "./research.type"
+import { EditResearchParams, EditResearchResponse, GetByIdResponse } from "./research.type"
 
 export const ResearchService={
   getById(id:number){
     return apiClient.get<GetByIdResponse>(`${RESEARCH.GETBYID}/${id}`)
+  },
+  edit(id:number,params:EditResearchParams){
+    return apiClient.put<EditResearchResponse>(`${RESEARCH.GETBYID}/${id}`,params)
   }
 }
--- src/features/research/research.type.ts
+++ src/features/research/research.type.ts
@@ -21,3 +21,19 @@ export interface GetByIdResponse {
     body: ResearchItem[];
   };
 }
+export type EditResearchParams = {
+  name: string;
+  description: string;
+  year: number;
+  fileUrl: string;
+  userId: number;
+  member: boolean;
+  univerName: string;
+  finished: boolean;
+  memberEnum: "MILLIY" | "XALQARO";
+}
+export type EditResearchResponse={
+  success: true,
+  message: string,
+  data: string
+}
